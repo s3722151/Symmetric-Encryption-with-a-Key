a48beda8cfa4a19e43e11eb5a556e4046604088b
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-01-05 16:11:27 +1100

    Resolved conflicts by importing classes.

--- src/symetricEncryptionKey.java
+++ src/symetricEncryptionKey.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.security.SecureRandom;
+import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.xml.bind.DatatypeConverter;
 
 public class symetricEncryptionKey {
@@ -26,13 +28,64 @@ public class symetricEncryptionKey {
     {
  
         // Used with encryption
-        byte[] initializationVector
-            = new byte[16];
-        SecureRandom secureRandom
-            = new SecureRandom();
+        byte[] initializationVector = new byte[16];
+        SecureRandom secureRandom = new SecureRandom();
         secureRandom.nextBytes(initializationVector);
         return initializationVector;
     }
+
+    // This function takes plaintext,
+    // the key with an initialization
+    // vector to convert plainText
+    // into CipherText.
+    public static byte[] do_AESEncryption(
+        String plainText,
+        SecretKey secretKey,
+        byte[] initializationVector)
+        throws Exception
+    {
+        Cipher cipher= Cipher.getInstance(
+                AES_CIPHER_ALGORITHM);
+ 
+        IvParameterSpec ivParameterSpec
+            = new IvParameterSpec(
+                initializationVector);
+ 
+        cipher.init(Cipher.ENCRYPT_MODE,
+                    secretKey,
+                    ivParameterSpec);
+ 
+        return cipher.doFinal(
+            plainText.getBytes());
+    }
+ 
+    // This function performs the
+    // reverse operation of the
+    // do_AESEncryption function.
+    // It converts ciphertext to
+    // the plaintext using the key.
+    public static String do_AESDecryption(
+        byte[] cipherText,
+        SecretKey secretKey,
+        byte[] initializationVector)
+        throws Exception
+    {
+        Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+ 
+        IvParameterSpec ivParameterSpec
+            = new IvParameterSpec(
+                initializationVector);
+ 
+        cipher.init(
+            Cipher.DECRYPT_MODE,
+            secretKey,
+            ivParameterSpec);
+ 
+        byte[] result
+            = cipher.doFinal(cipherText);
+ 
+        return new String(result);
+    }    
 	
 //Main Method
 	public static void main(String[] args) {
