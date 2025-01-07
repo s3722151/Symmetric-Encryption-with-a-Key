0035f271c3abda3e889ed7a2f9d548ed78629264
Zetazone <37312796+Zetazone@users.noreply.github.com>
2025-01-07 18:04:14 +1100

    Just changed the main file based on Demo1 file.

--- src/symetricEncryptionKey.java
+++ src/symetricEncryptionKey.java
@@ -1,115 +1,58 @@
-import java.util.Scanner;  // Import the Scanner class
-import java.security.SecureRandom;
+import java.security.Key;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
-import javax.xml.bind.DatatypeConverter;
+import java.util.Scanner;  // Import the Scanner class
+
 
 public class symetricEncryptionKey {
-//Methods
-    private static final String AES = "AES";
-	// We are using a Block cipher(CBC mode)
-	private static final String AES_CIPHER_ALGORITHM = "AES/CBC/PKCS5PADDING";
-	private static Scanner message;
+	public static void main(String[] args) throws Exception {
 	
-    // Function to create a secret key
-    public static SecretKey createAESKey()
-        throws Exception
-    {
-        SecureRandom securerandom = new SecureRandom();
-        KeyGenerator keygenerator = KeyGenerator.getInstance(AES);
-        keygenerator.init(256, securerandom);
-        SecretKey key = keygenerator.generateKey();
-        return key;
-    }
-    // Function to initialize a vector with an arbitrary value
-    public static byte[] createInitializationVector()
-    {
- 
-        // Used with encryption
-        byte[] initializationVector = new byte[16];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(initializationVector);
-        return initializationVector;
-    }
-
-    // This function takes plaintext,
-    // the key with an initialization
-    // vector to convert plainText
-    // into CipherText.
-    public static byte[] do_AESEncryption(
-        String plainText,
-        SecretKey secretKey,
-        byte[] initializationVector)
-        throws Exception
-    {
-        Cipher cipher= Cipher.getInstance(
-                AES_CIPHER_ALGORITHM);
- 
-        IvParameterSpec ivParameterSpec
-            = new IvParameterSpec(
-                initializationVector);
- 
-        cipher.init(Cipher.ENCRYPT_MODE,
-                    secretKey,
-                    ivParameterSpec);
- 
-        return cipher.doFinal(
-            plainText.getBytes());
-    }
- 
-    // This function performs the
-    // reverse operation of the
-    // do_AESEncryption function.
-    // It converts ciphertext to
-    // the plaintext using the key.
-    public static String do_AESDecryption(
-        byte[] cipherText,
-        SecretKey secretKey,
-        byte[] initializationVector)
-        throws Exception
-    {
-        Cipher cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
- 
-        IvParameterSpec ivParameterSpec
-            = new IvParameterSpec(
-                initializationVector);
- 
-        cipher.init(
-            Cipher.DECRYPT_MODE,
-            secretKey,
-            ivParameterSpec);
- 
-        byte[] result
-            = cipher.doFinal(cipherText);
- 
-        return new String(result);
-    }    
-	
-//Main Method
-	public static void main(String[] args) {
-		//Input Phase: The user provides a text string and a shared key. Example: Input text is "Confidential Message" and the key is "MySecureKey!".
+    	//Step 0: Take in user input
 	    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("Enter a text string");
 	    String textString = myObj.nextLine();  // Read user input
 	    System.out.println("The unencrypted string is: " + textString);
-	    
-	    
-	    //Key generation phase:
-	    
 
-	    //Encryption Phase: The text is encrypted using the shared key. Example: The program transforms "Confidential Message" into "3cL7pQ==%!".
-	    System.out.println("The encrypted string is: ");
+        // Step 1: Convert the text to bytes.
+        //byte[] message = "Hello, World!".getBytes();
+        byte[] message = textString.getBytes();
 
+        // Step 2: Create a KeyGenerator object
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
 
-	    //Decryption Phase: The encrypted text is decrypted back into its original form using the same key. Example: "3cL7pQ==%!" is decrypted to "Confidential Message".
-	    System.out.println("The decrypted string is: ");
+        // Step 3: Initialize the KeyGenerator with a keysize
+        keyGen.init(256);
+
+        // Step 4: Generate the key
+        Key key = keyGen.generateKey();
+
+        // Step 5: Create a Cipher object
+        Cipher cipher = Cipher.getInstance("AES");
+
+        // Step 6: Initialize the Cipher object
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        // Step 7: Give the Cipher our message
+        cipher.update(message);
+
+        // Step 8: Encrypt the message
+        byte[] ciphertext = cipher.doFinal();
+
+        // Step 9: Print the ciphertext
+        System.out.println("message: " + new String(message, "UTF8"));
+        System.out.println("ciphertext: " + new String(ciphertext, "UTF8"));
+
+        // Step 10: Change the Cipher object's mode
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        // Step 11: Give the Cipher objectour ciphertext
+        cipher.update(ciphertext);
+
+        // Step 12: Decrypt the ciphertext
+        byte[] decrypted = cipher.doFinal();
+        System.out.println("Cipher text that was decrypted is: " + new String(decrypted, "UTF8"));
 	}//Main Method End
 }//Class End
-
-
-
 
 
 /*
@@ -119,9 +62,13 @@ What is AES: https://www.techtarget.com/searchsecurity/definition/Advanced-Encry
 	What are data leaks: https://abnormalsecurity.com/glossary/data-leak
 	Difference between Symmetric and asymmetric data: https://www.youtube.com/watch?v=nRou-J4YN4Y
 
-How to do Symmetric Encryption: https://www.geeksforgeeks.org/symmetric-encryption-cryptography-in-java/
+
+How to do Symmetric Encryption: https://gregorycernera.medium.com/encrypting-and-decrypting-a-message-using-symmetric-keys-with-java-explained-step-by-step-with-a523b67877d8
+	Broken example: https://www.geeksforgeeks.org/symmetric-encryption-cryptography-in-java/
 	Complicated example: https://www.baeldung.com/java-aes-encryption-decryption
 
 Notes
 - AES is a form of symmetric encryption 
+- Symmetric encryption is good if you decryot with private key
+
 */
